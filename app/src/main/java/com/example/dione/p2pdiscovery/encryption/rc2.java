2b760e0d8c916a9e49b3a9bec0ea89d6186181e9
Clinton Dcruz <clint.dcruze@outlook.com>
2017-04-14 11:51:50 +0530

    Blowfish

--- app/src/main/java/com/example/dione/p2pdiscovery/encryption/rc2.java
+++ app/src/main/java/com/example/dione/p2pdiscovery/encryption/rc2.java
@@ -1,42 +1,25 @@
 package com.example.dione.p2pdiscovery.encryption;
-import javax.crypto.spec.*;
-import java.security.*;
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class rc2 {
+    byte[] text;
+    Cipher cipher;
+    SecretKeySpec KS;
 
-    public static byte[] encrypt(String toEncrypt, String key) throws Exception {
-        // create a binary key from the argument key (seed)
-        SecureRandom sr = new SecureRandom(key.getBytes());
-        KeyGenerator kg = KeyGenerator.getInstance("RC2");
-        kg.init(sr);
-        SecretKey sk = kg.generateKey();
-
-        // create an instance of cipher
-        Cipher cipher = Cipher.getInstance("RC2");
-
-        // initialize the cipher with the key
-        cipher.init(Cipher.ENCRYPT_MODE, sk);
-
-        // enctypt!
-        byte[] encrypted = cipher.doFinal(toEncrypt.getBytes());
-
-        return encrypted;
+    public rc2() throws Exception{
+        String Key = "pockets.xie.engg";
+        byte[] KeyData = Key.getBytes();
+        KS = new SecretKeySpec(KeyData, "Blowfish");
+        cipher = Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
     }
 
-    public static String decrypt(byte[] toDecrypt, String key) throws Exception {
-        // create a binary key from the argument key (seed)
-        SecureRandom sr = new SecureRandom(key.getBytes());
-        KeyGenerator kg = KeyGenerator.getInstance("RC2");
-        kg.init(sr);
-        SecretKey sk = kg.generateKey();
-
-        // do the decryption with that key
-        Cipher cipher = Cipher.getInstance("RC2");
-        cipher.init(Cipher.DECRYPT_MODE, sk);
-        byte[] decrypted = cipher.doFinal(toDecrypt);
-
-        return new String(decrypted);
+    public byte[] process(byte[] message, String mode) throws Exception{
+        if(mode.equals("-e"))
+            cipher.init(Cipher.ENCRYPT_MODE, KS);
+        else if(mode.equals("-d"))
+            cipher.init(Cipher.DECRYPT_MODE, KS);
+        text = cipher.doFinal(message);
+        return text;
     }
-
-    }
+}
